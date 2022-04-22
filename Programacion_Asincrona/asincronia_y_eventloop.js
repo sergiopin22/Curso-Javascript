95f3b8ab4207cde28bea2eb89145fa0458415c18
sergiopin22 <sergiolamoney@gmail.com>
2022-04-22 15:24:48 -0500

    Subiendo primeros pasos asincronia en js

--- Programacion_Asincrona/asincronia_y_eventloop.js
+++ Programacion_Asincrona/asincronia_y_eventloop.js
@@ -11,9 +11,43 @@
 // CODIGO SINCRONO BLOQUEANTE 
 (() =>{
 
+    console.log("codigo sincrono")
+    console.log("incio")
+    function dos(){
+        console.log("dos")
+    }
+    function uno(){
+        console.log("uno")
+        dos()
+        console.log("tres")
+    }
+    uno();
+    console.log("Fin")
+    console.log("***************************")
 })();
 // CODIGO ASINCRONO NO BLOQUEANTE
 (() =>{
+    console.log("Codigo Asincrono")
+    console.log("Inicio")
+
+    function dos(){
+        setTimeout(function() {
+            console.log("Dos")
+        }, 1000);
+    }
+
+    function uno(){
+        setTimeout(function() {
+            console.log("Uno")
+        }, 0);
+        dos();
+        console.log("Tres")
+    }
+
+    uno();
+    console.log("Fin")
 
 })();
 
+// _JAVASCRIPT USA UN MODELO ASINCRONO Y NO BLOQUEANTE CON UN LOOP DE EVENTOS(EVENT LOOP) IMPLEMENTANDO EN UN SOLO 
+// _HILO (SINGLE THREAD) PARA OPERACIONES DE ENTRADA Y SALIDA (INPUT/OUTPUT)
