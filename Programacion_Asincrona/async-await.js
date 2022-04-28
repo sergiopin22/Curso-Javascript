3dffe066bd4943def477523048d08310a55fea2b
sergiopin22 <sergiolamoney@gmail.com>
2022-04-27 20:56:43 -0500

    subiendo async and await

--- Programacion_Asincrona/async-await.js
+++ Programacion_Asincrona/async-await.js
@@ -17,9 +17,9 @@ async function retornarNumero(){
 
 // _Al ejecutar la función veremos que ya nos devuelve una  que ha sido cumplida, con el valor devuelto en la función (en este caso, 42). De hecho, podríamos utilizar un .then() para manejar la promesa
 
-retornarNumero().then(value => {
-    console.log(`el numero retornado en la funcion anterior es : "${value}"`)
-});
+// retornarNumero().then(value => {
+//     console.log(`el numero retornado en la funcion anterior es : "${value}"`)
+// });
 
 // !!!Sin embargo, veremos que lo que se suele hacer junto a async es utilizar la palabra clave await, que es donde reside lo interesante de utilizar este enfoque.
 
@@ -42,13 +42,56 @@ function cuadradoPromise(value){
 async function funcionAsincronaDeclarada(){
     try{
         console.log("Inicio Async Function")
-        let obj = cuadradoPromise(0);
-        console.log(`Async Function ${obj.value}, ${obj.result}`)
-        obj = cuadradoPromise(1);
-        console.log(`Async Function ${obj.value}, ${obj.result}`)
-    }catch(err){
+        let obj = await cuadradoPromise(0);// con la palabra await indicamos al programa que espere la respuesta de la funcion para poder pasar a la siguiente linea de codigo de lo contrario no.
 
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(1);
+        
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(2);
+
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(3);
+
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(4);
+
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(5);
+
+        console.log("Fin Asyn Function")
+    }catch(err){
+        console.error(err)
     }
 }
 
 funcionAsincronaDeclarada();
+
+const funcionAsincronaExpresada = async () =>{
+    try{
+        console.log("Inicio Async Function")
+        let obj = await cuadradoPromise(0);// con la palabra await indicamos al programa que espere la respuesta de la funcion para poder pasar a la siguiente linea de codigo de lo contrario no.
+
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(7);
+        
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(8);
+
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(9);
+
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(10);
+
+        console.log(`Async Function ${obj.value}, ${obj.result}`)
+        obj =  await cuadradoPromise(11);
+
+        console.log("Fin Asyn Function")
+    }catch(err){
+        console.error(err)
+    }
+}
+
+funcionAsincronaExpresada();
+
